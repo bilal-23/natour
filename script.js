9c1189ee11a1d4b84de6f7f888141f64504e103a
bilal-23 <mansuribilal101@gmail.com>
2021-04-15 21:09:51 +0530

    reveal section commented

--- script.js
+++ script.js
@@ -11,23 +11,23 @@ navLinkList.addEventListener('click', function (e) {
 
 //Reveal sections
 
-const allSections = document.querySelectorAll('.section')
+// const allSections = document.querySelectorAll('.section')
 
-const revealSection = function (entries, observer) {
-    const [entry] = entries;
-    console.log(entry);
+// const revealSection = function (entries, observer) {
+//     const [entry] = entries;
+//     console.log(entry);
 
-    if (!entry.isIntersecting) return;
+//     if (!entry.isIntersecting) return;
 
-    entry.target.classList.remove("section--hidden");
-    sectionObserver.unobserve(entry.target)
-}
-const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.25,
-});
+//     entry.target.classList.remove("section--hidden");
+//     sectionObserver.unobserve(entry.target)
+// }
+// const sectionObserver = new IntersectionObserver(revealSection, {
+//     root: null,
+//     threshold: 0.25,
+// });
 
-allSections.forEach(section => {
-    sectionObserver.observe(section);
-    section.classList.add('section--hidden')
-})
+// allSections.forEach(section => {
+//     sectionObserver.observe(section);
+//     section.classList.add('section--hidden')
+// })
